536d6ef078bcf944797d449c31b37a234c9dabc5
Mark Newnham <mark@newnhams.com>
2025-10-08 21:35:48 -0600

    Fix data if a full application test

--- TransactionScopeTest.php
+++ TransactionScopeTest.php
@@ -35,6 +35,28 @@ class TransactionScopeTest extends ADOdbTestCase
     protected bool $skipFollowingTests = false;
 
     /**
+     * Set up the test environment first time
+     *
+     * @return void
+     */
+    public static function setupBeforeClass(): void
+    {
+
+        $db        = $GLOBALS['ADOdbConnection'];
+        $adoDriver = $GLOBALS['ADOdriver'];
+
+        /*
+        * Fixes previously damaged transactions if necessary
+        */
+        $db->startTrans();
+        $SQL = "UPDATE testtable_3 SET varchar_field='LINE 1' WHERE varchar_field IS NULL";
+        $db->execute($SQL);
+        $db->completeTrans();
+       
+    }
+
+
+    /**
      * Set up the test environment
      *
      * @return void
@@ -52,7 +74,6 @@ class TransactionScopeTest extends ADOdbTestCase
         }
     }
 
-
     /**
      * Tests the smart transaction handling capabilities
      *
@@ -60,6 +81,7 @@ class TransactionScopeTest extends ADOdbTestCase
      */
     public function testStartCompleteTransaction(): void
     {
+        
         if ($this->skipFollowingTests) {
             $this->markTestSkipped(
                 'Skipping testStartCompleteTransaction as it ' .
@@ -67,138 +89,210 @@ class TransactionScopeTest extends ADOdbTestCase
             );
         }
 
-        if (ADODB_ASSOC_CASE == ADODB_ASSOC_CASE_UPPER) {
-            $idField = 'ID';
-            $vcField = 'VARCHAR_FIELD';
-        } else {
-            $idField = 'id';
-            $vcField = 'varchar_field';
-        }
+        foreach ($this->testFetchModes as $fetchMode => $fetchDescription) {
 
-        $this->db->StartTrans();
+            $this->db->setFetchMode($fetchMode);
 
+            if ($fetchMode == ADODB_FETCH_NUM) {
+                $idField = 0;
+                $vcField = 1;
+            }
+            else if (ADODB_ASSOC_CASE == ADODB_ASSOC_CASE_UPPER) {
+                $idField = 'ID';
+                $vcField = 'VARCHAR_FIELD';
+            } else {
+                $idField = 'id';
+                $vcField = 'varchar_field';
+            }
 
-        $assertion = $this->assertEquals(
-            1,
-            $this->db->transOff,
-            'Transaction did not start correctly, ' .
-            'transOff should be greater than 0'
-        );
+            $this->db->StartTrans();
 
-
-        $sql = "SELECT id, varchar_field 
-                  FROM testtable_3 
-              ORDER BY id";
-
-        $baseData = $this->db->getRow($sql);
-        list($errno, $errmsg) = $this->assertADOdbError($sql);
-
-        if ($errno > 0) {
-            return;
-        }
-
-        $sql = "UPDATE testtable_3 
-                   SET varchar_field = 'transaction test' 
-                 WHERE id = 1";
-
-        list($result, $errno, $errmsg) = $this->executeSqlString($sql, null, false);
-
-        if ($errno > 0) {
-            return;
-        }
-
-
-
-        /*
-        * Check that the data has been updated in the transaction
-        */
-        $sql = "SELECT varchar_field FROM testtable_3 WHERE id = {$baseData[$idField]}";
-        $preCommit = $this->db->getOne($sql);
-        list($errno, $errmsg) = $this->assertADOdbError($sql);
-
-        if ($errno > 0) {
-            return;
-        }
-
-        $this->assertEquals(
-            'transaction test',
-            $preCommit,
-            'Data should be updated in the transaction'
-        );
-
-        /*
-        * Now we will rollback the transaction
-        */
-        $this->db->rollbackTrans();
-        list($errno, $errmsg) = $this->assertADOdbError('rollbackTrans()');
-
-        if ($errno > 0) {
-            return;
-        }
-
-        $this->assertEquals(
-            1,
-            $this->db->transOff,
-            'Transaction count still should be 1 after rolling back the ' .
-            'transaction but before the completeTrans()'
-        );
-
-        $sql = "SELECT varchar_field FROM testtable_3 WHERE id = {$baseData[$idField]}";
-        $postRollback = $this->db->getOne($sql);
-
-        list($errno, $errmsg) = $this->assertADOdbError($sql);
-
-        if ($errno > 0) {
-            return;
-        }
-
-        $this->assertEquals(
-            'transaction test',
-            $postRollback,
-            'Data should still be the updated value ' .
-            'after rolling back the transaction'
-        );
-
-        $this->db->CompleteTrans();
-        list($errno, $errmsg) = $this->assertADOdbError('completeTrans()');
-
-        if ($errno > 0) {
-            return;
-        }
-        $assertion = $this->assertEquals(
-            0,
-            $this->db->transOff,
-            'Transaction count $transOff should now equal 0 ' .
-            'after completing the transaction'
-        );
-
-        if ($this->db->transOff <> 0) {
-            $this->fail(
+            $assertion = $this->assertEquals(
+                1,
+                $this->db->transCnt,
                 sprintf(
-                    'Trans Count shoud be 0 but is %d',
-                    $this->db->transOff
+                    '[%s] Transaction did not start correctly, ' .
+                    'transCnt should be equal to 1',
+                    $fetchDescription
                 )
             );
-            return;
+
+            $sql = "SELECT id, varchar_field
+                      FROM testtable_3 
+                  ORDER BY id";
+
+            $baseData = $this->db->getRow($sql);
+
+            list($errno, $errmsg) = $this->assertADOdbError($sql);
+
+            if ($errno > 0) {
+                return;
+            }
+
+            $this->assertSame(
+                $baseData[$vcField],
+                'LINE 1',
+                sprintf(
+                    '[%s] Test should start with field initialized to "LINE 1"',
+                    $fetchDescription
+                )
+            );
+
+            $sql = "UPDATE testtable_3 
+                    SET varchar_field = 'transaction test' 
+                    WHERE id = 1";
+
+            list($result, $errno, $errmsg) = $this->executeSqlString($sql, null, false);
+
+            if ($errno > 0) {
+                $this->failTest(
+                   sprintf(
+                        '[%s] Cannot set varchar_field error [%s] %s',
+                        $fetcDescription,
+                        $errno,
+                        $errmsg
+                    )
+                );
+                return;
+            }
+
+            /*
+            * Check that the data has been updated in the transaction
+            */
+            $sql = "SELECT varchar_field 
+                    FROM testtable_3 
+                    WHERE id = {$baseData[$idField]}";
+            $preCommit = $this->db->getOne($sql);
+            
+            list($errno, $errmsg) = $this->assertADOdbError($sql);
+
+            if ($errno > 0) {
+                return;
+            }
+
+            $this->assertEquals(
+                'transaction test',
+                $preCommit,
+                sprintf(
+                    '[%s] Data should be updated in the transaction',
+                    $fetchDescription
+                )
+            );
+
+            /*
+            * Now we will rollback the transaction
+            */
+            $this->assertEquals(
+                1,
+                $this->db->transOff,
+                sprintf(
+                    '[%s] Smart Transactions should not ' . 
+                    'be interspersed woth standard transactions',
+                    $fetchDescription
+                )
+            );
+            
+
+            /*
+            * Test the transaction to rollback at completion
+            */
+            $this->db->failTrans();
+
+            list($errno, $errmsg) = $this->assertADOdbError('failTrans()');
+
+            if ($errno > 0) {
+                return;
+            }
+
+            $this->assertEquals(
+                1,
+                $this->db->transCnt,
+                sprintf(
+                    '[%s] Transaction count still should be 1 after rolling back the ' .
+                    'transaction but before the completeTrans()',
+                    $fetchDescription
+                )
+            );
+
+            $sql = "SELECT varchar_field 
+                    FROM testtable_3 
+                    WHERE id = {$baseData[$idField]}";
+            
+            $postRollback = $this->db->getOne($sql);
+
+            list($errno, $errmsg) = $this->assertADOdbError($sql);
+
+            if ($errno > 0) {
+                return;
+            }
+
+            $this->assertEquals(
+                'transaction test',
+                $postRollback,
+                sprintf(
+                    '[%s] Data should still be the updated value ' .
+                    'after rolling back the transaction',
+                    $fetchDescription
+                )
+            );
+
+            $transactionFailed = $this->db->hasFailedTrans();
+
+            $this->assertTrue(
+                $transactionFailed,
+                'hasFailedTrans() should report true'
+            );
+
+
+            $this->db->CompleteTrans();
+            list($errno, $errmsg) = $this->assertADOdbError('completeTrans()');
+
+            if ($errno > 0) {
+                return;
+            }
+            
+            $assertion = $this->assertEquals(
+                0,
+                $this->db->transCnt,
+                sprintf(
+                    '[%s] Transaction count $transCnt should now equal 0 ' .
+                    'after completing the transaction',
+                    $fetchDescription
+                )
+            );
+
+            if ($this->db->transCnt > 0) {
+                $this->fail(
+                    sprintf(
+                        '[%s] Trans Count shoud be 0 but is %d',
+                        $fetcDescription,
+                        $this->db->transCnt
+                    )
+                );
+                return;
+            }
+
+            $sql = "SELECT varchar_field 
+                    FROM testtable_3 
+                    WHERE id = {$baseData[$idField]}";
+
+            $postCommit = $this->db->getOne($sql);
+            list($errno, $errmsg) = $this->assertADOdbError($sql);
+
+            if ($errno > 0) {
+                return;
+            }
+
+            $this->assertEquals(
+                $baseData[$vcField],
+                $postCommit,
+                sprintf(
+                    '[%s] Data should now be reverted to the original ' .
+                    'value after committing the transaction',
+                    $fetchDescription
+                )
+            );
         }
-
-        $sql = "SELECT varchar_field 
-                  FROM testtable_3 
-                 WHERE id = {$baseData[$idField]}";
-
-        $postCommit = $this->db->getOne($sql);
-        list($errno, $errmsg) = $this->assertADOdbError($sql);
-
-        if ($errno > 0) {
-            return;
-        }
-
-        $this->assertEquals(
-            $baseData[$vcField],
-            $postCommit,
-            'Data should still be reverted to the original ' .
-            'value after committing the transaction'
-        );
     }
 
     /**
@@ -215,88 +309,111 @@ class TransactionScopeTest extends ADOdbTestCase
             );
         }
 
-        if (ADODB_ASSOC_CASE == ADODB_ASSOC_CASE_UPPER) {
-            $idField = 'ID';
-            $vcField = 'VARCHAR_FIELD';
-        } else {
-            $idField = 'id';
-            $vcField = 'varchar_field';
+        foreach ($this->testFetchModes as $fetchMode => $fetcDescription) {
+
+            $this->db->setFetchMode($fetchMode);
+
+            if ($fetchMode == ADODB_FETCH_NUM) {
+                $idField = 0;
+                $vcField = 1;
+            }
+            else if (ADODB_ASSOC_CASE == ADODB_ASSOC_CASE_UPPER) {
+                $idField = 'ID';
+                $vcField = 'VARCHAR_FIELD';
+            } else {
+                $idField = 'id';
+                $vcField = 'varchar_field';
+            }
+
+            $this->db->beginTrans();
+
+            $assertion = $this->assertEquals(
+                1,
+                $this->db->transCnt,
+                sprintf(
+                    '[%s] Transaction did not start correctly,' .
+                    'transCnt should be equal to 1',
+                    $fetcDescription
+                 )
+            );
+
+            $sql = "SELECT id, varchar_field 
+                    FROM testtable_3 
+                ORDER BY id";
+
+            $baseData = $this->db->getRow($sql);
+            list($errno, $errmsg) = $this->assertADOdbError($sql);
+
+            if ($errno > 0) {
+                return;
+            }
+
+
+            $sql = "UPDATE testtable_3 
+                    SET varchar_field = 'transaction test' 
+                    WHERE id = 1";
+
+            $this->db->execute($sql);
+            list($result, $errno, $errmsg) = $this->executeSqlString($sql, null, false);
+
+            if ($errno > 0) {
+                return;
+            }
+
+            /*
+            * Check that the data has been updated in the transaction
+            */
+            $sql = "SELECT varchar_field 
+                    FROM testtable_3 
+                    WHERE id = {$baseData[$idField]}";
+            $preCommit = $this->db->getOne($sql);
+
+            list($errno, $errmsg) = $this->assertADOdbError($sql);
+
+            if ($errno > 0) {
+                return;
+            }
+
+            $this->assertEquals(
+                'transaction test',
+                $preCommit,
+                 sprintf(
+                    '[%s] VARCHAR_FIELD Data should have been updated ' .
+                    'in the transaction before commit',
+                    $fetcDescription
+                 )
+            );
+
+            $this->db->rollbackTrans();
+
+            
+            $this->db->CommitTrans();
+            
+            
+            list($errno, $errmsg) = $this->assertADOdbError('commitTrans()');
+            /*
+            * Check that the data has been rolled back in the transaction
+            */
+            $sql = "SELECT varchar_field 
+                    FROM testtable_3 
+                    WHERE id = {$baseData[$idField]}";
+
+            $postCommit = $this->db->getOne($sql);
+
+            list($errno, $errmsg) = $this->assertADOdbError($sql);
+
+            if ($errno > 0) {
+                return;
+            }
+            $this->assertEquals(
+                $baseData[$vcField],
+                $postCommit,
+                 sprintf(
+                    '[%s] VARCHAR_FIELD Data should now be rolled back ' .
+                    'in the transaction after commit',
+                    $fetcDescription
+                 )
+            );
         }
-
-        $this->db->beginTrans();
-
-        $assertion = $this->assertEquals(
-            1,
-            $this->db->transCnt,
-            'Transaction did not start correctly,' .
-            'transCnt should be equal to 1'
-        );
-
-        $sql = "SELECT id, varchar_field 
-                  FROM testtable_3 
-              ORDER BY id";
-
-        $baseData = $this->db->getRow($sql);
-        list($errno, $errmsg) = $this->assertADOdbError($sql);
-
-        if ($errno > 0) {
-            return;
-        }
-
-
-        $sql = "UPDATE testtable_3 
-                   SET varchar_field = 'transaction test' 
-                 WHERE id = 1";
-
-        $this->db->execute($sql);
-        list($result, $errno, $errmsg) = $this->executeSqlString($sql, null, false);
-
-        if ($errno > 0) {
-            return;
-        }
-
-        /*
-        * Check that the data has been updated in the transaction
-        */
-        $sql = "SELECT varchar_field 
-                  FROM testtable_3 
-                 WHERE id = {$baseData[$idField]}";
-        $preCommit = $this->db->getOne($sql);
-
-        list($errno, $errmsg) = $this->assertADOdbError($sql);
-
-        if ($errno > 0) {
-            return;
-        }
-
-        $this->assertEquals(
-            $baseData[$vcField],
-            $preCommit,
-            'VARCHAR_FIELD Data should not yet be updated ' .
-            'in the transaction before commit'
-        );
-
-        $this->db->CommitTrans();
-        list($errno, $errmsg) = $this->assertADOdbError('commitTrans()');
-        /*
-        * Check that the data has been updated in the transaction
-        */
-        $sql = "SELECT varchar_field 
-                  FROM testtable_3 
-                 WHERE id = {$baseData[$idField]}";
-
-        $postCommit = $this->db->getOne($sql);
-
-        list($errno, $errmsg) = $this->assertADOdbError($sql);
-
-        if ($errno > 0) {
-            return;
-        }
-        $this->assertEquals(
-            'transaction test',
-            $postCommit,
-            'VARCHAR_FIELD Data should now be updated ' .
-            'in the transaction after commit'
-        );
     }
 }
