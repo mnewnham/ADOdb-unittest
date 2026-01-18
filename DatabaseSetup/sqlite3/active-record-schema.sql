e3813fc6f71b9daa816fbec5b235dd2c1c765e36
Mark Newnham <mark@newnhams.com>
2026-01-17 19:40:55 -0700

    Added the Active Record Library for sqlite3

---
+++ DatabaseSetup/sqlite3/active-record-schema.sql
@@ -0,0 +1,19 @@
+-- Test tables for the active record module
+
+DROP TABLE IF EXISTS persons;
+DROP TABLE IF EXISTS children;
+
+CREATE TABLE persons (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name_first VARCHAR(100) NOT NULL,
+    name_last VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+);
+
+CREATE TABLE children (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    person_id INTEGER NOT NULL,
+    name_first VARCHAR(100) NOT NULL,
+    name_last VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL
+);
