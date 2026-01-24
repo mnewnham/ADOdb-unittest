cec6fbbd3154fef598ebd0c9801999754095881d
Mark Newnham <mark@newnhams.com>
2026-01-24 11:08:37 -0700

    Trailing comma error in definition

--- tools/DatabaseSetup/sqlite3/active-record-schema.sql
+++ tools/DatabaseSetup/sqlite3/active-record-schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE persons (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name_first VARCHAR(100) NOT NULL,
     name_last VARCHAR(100) NOT NULL,
-    birth_date DATE NOT NULL,
+    birth_date DATE NOT NULL
 );
 
 CREATE TABLE children (
