e0097d58f5c04c8d6f0376714a263ce046e9c746
Mark Newnham <mark@newnhams.com>
2026-02-06 10:25:51 -0700

    Adds an initial dictionary test file format

--- tools/DatabaseSetup/mysqli/table-schema.sql
+++ tools/DatabaseSetup/mysqli/table-schema.sql
@@ -86,3 +86,20 @@ CREATE TABLE `table_name` (
 	`column_name` VARCHAR(20),
 	PRIMARY KEY(`id`)
 ) ENGINE=INNODB;
+
+-- This table is used to test changes to columns via dictionary
+-- functions. not used by createTableSql
+
+DROP TABLE IF EXISTS dictionary_change_test_table;
+
+CREATE TABLE dictionary_change_test_table (
+	id INT NOT NULL AUTO_INCREMENT,
+	date_field DATE NOT NULL DEFAULT '2030-01-01',
+	integer_field INT DEFAULT 0,
+	decimal_field_to_modify DECIMAL(8.4) NOT NULL DEFAULT 0,
+	boolean_field_to_rename BOOLEAN DEFAULT 0,
+	droppable_field decimal(10.6) NOT NULL DEFAULT 80.111,
+	enum_field_to_keep ENUM('duplo','lego','meccano'),
+	varchar_field VARCHAR(50),
+	PRIMARY KEY(id)
+);
