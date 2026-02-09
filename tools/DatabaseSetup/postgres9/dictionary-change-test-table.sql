ebb4e9232eb2a8ec9f008b9b099fa793b4cc6ab1
Mark Newnham <mark@newnhams.com>
2026-02-09 15:50:31 -0700

    Add bool control for postgres

--- tools/DatabaseSetup/postgres9/dictionary-change-test-table.sql
+++ tools/DatabaseSetup/postgres9/dictionary-change-test-table.sql
@@ -6,16 +6,17 @@ DROP TABLE IF EXISTS dictionary_change_test_table;
 
 -- Missing ENUM support until added to ADOdb
 -- enum_field_to_keep ENUM('duplo','lego','meccano'),
+-- No direct NVARCHAR type
 
 CREATE TABLE dictionary_change_test_table (
 	id SERIAL PRIMARY KEY,
 	date_field DATE NOT NULL DEFAULT '2030-01-01',
 	integer_field SMALLINT DEFAULT 0,
 	decimal_field_to_modify DECIMAL(8,4) DEFAULT 0.0,
-	boolean_field_to_rename BOOLEAN,
+	boolean_field_to_rename BOOLEAN DEFAULT FALSE,
     droppable_field decimal(10,6) NOT NULL DEFAULT 80.111,
-	varchar_field VARCHAR(50) DEFAULT ''
-	nvarchar_field NVARCHAR(50) DEFAULT ''
+	varchar_field VARCHAR(50) DEFAULT '',
+	nvarchar_field VARCHAR(50) DEFAULT ''
 );
 CREATE UNIQUE INDEX index_to_drop ON dictionary_change_test_table (varchar_field);
 
