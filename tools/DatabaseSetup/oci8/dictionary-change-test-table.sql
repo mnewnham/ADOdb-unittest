f5bf09b30173d04967599f75832341755657315a
Mark Newnham <mark@newnhams.com>
2026-02-13 17:14:07 -0700

    No native ENUM suppoer for oracle

--- tools/DatabaseSetup/oci8/dictionary-change-test-table.sql
+++ tools/DatabaseSetup/oci8/dictionary-change-test-table.sql
@@ -1,25 +1,28 @@
 -- This table is used to test changes to columns via dictionary
 -- functions. not used by createTableSql
 -- Reprocessed for each test to provide a standard baseline
-
+-- enum_field_to_keep ENUM('duplo','lego','meccano'), //NOT AVAILABLE IN ORACLE
 DROP TABLE IF EXISTS dictionary_change_test_table;
+DROP SEQUENCE IF EXISTS dictionary_change_test_table_seq;
+DROP TRIGGER IF EXISTS dictionary_change_test_table_t;
 
 CREATE TABLE dictionary_change_test_table (
 	id INTEGER NOT NULL ,
-	date_field DATE NOT NULL DEFAULT '2030-01-01',
+	date_field DATE NOT NULL,
 	integer_field SMALLINT DEFAULT 0,
-	decimal_field_to_modify NUMBER(8,4) NOT NULL DEFAULT 0,
+	decimal_field_to_modify NUMBER(8,4) DEFAULT 0,
 	boolean_field_to_rename NUMBER(1,0),
 	boolean_field_to_change_default NUMBER(1,1),
-	droppable_field NUMBER(10,6) NOT NULL DEFAULT 80.111,
-	enum_field_to_keep ENUM('duplo','lego','meccano'),
+	droppable_field NUMBER(10,6) NOT NULL,
+	
 	varchar_field VARCHAR(50),
-	nvarchar_field NVARCHAR(50),
+	nvarchar_field NVARCHAR2(50),
 	smallint_to_expand SMALLINT,
 	xl_field BLOB,
-	PRIMARY KEY(id),
-	UNIQUE INDEX index_to_drop (varchar_field)
+	PRIMARY KEY(id)
 );
+
+CREATE UNIQUE INDEX index_to_drop ON dictionary_change_test_table(varchar_field);
 
 CREATE SEQUENCE dictionary_change_test_table_seq
     INCREMENT BY 1
@@ -28,4 +31,6 @@ CREATE SEQUENCE dictionary_change_test_table_seq
 -- This statement has an extraneous ; at end to force 
 -- the procedure to be created in Oracle. It will be stripped
 -- by the schema loader
-CREATE OR REPLACE TRIGGER testable_1_t BEFORE insert ON dictionary_change_test_table FOR EACH ROW WHEN (NEW.id IS NULL OR NEW.id=0) BEGIN select dictionary_change_test_table_seq.nextval into :new.id from dual; END; ;
+-- DO NOT REMOVE
+
+CREATE OR REPLACE TRIGGER dictionary_change_test_table_t BEFORE insert ON dictionary_change_test_table FOR EACH ROW WHEN (NEW.id IS NULL OR NEW.id=0) BEGIN select dictionary_change_test_table_seq.nextval into :new.id from dual; END; ;
