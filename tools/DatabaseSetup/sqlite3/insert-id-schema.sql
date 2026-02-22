d1e0f07a5be83f0d841ad7add9deab294ca6c878
Mark Newnham <mark@newnhams.com>
2026-02-21 20:28:06 -0700

    Fix SQLite driver

--- tools/DatabaseSetup/sqlite3/insert-id-schema.sql
+++ tools/DatabaseSetup/sqlite3/insert-id-schema.sql
@@ -6,12 +6,12 @@ DROP TABLE IF EXISTS insert_manual;
 
 -- Creates a simple table that has an auto-increment key
 CREATE TABLE insert_auto (
-	id INTEGER PRIMARY AUTO_INCREMENT,
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	integer_field INTEGER NOT NULL
 );
 
 -- Creates a simple table where the key field must be incremented manually
 CREATE TABLE insert_manual (
-	id INTEGER PRIMARY,
+	id INTEGER PRIMARY KEY,
 	integer_field INTEGER NOT NULL
 );
